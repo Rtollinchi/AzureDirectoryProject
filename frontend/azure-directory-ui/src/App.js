d407c4d105422edc7eb37042828fb5615ad394b1
Rtollinchi <rtollinchi23@gmail.com>
2024-05-15 12:09:14 -0400

    Fixed invalid hook call errors by ensuring single React instance

--- frontend/azure-directory-ui/src/App.js
+++ frontend/azure-directory-ui/src/App.js
@@ -1,8 +1,23 @@
-import React from 'react';
-import Login from './Login';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Login from "./components/Login";
+import UserList from "./components/UserProfile";
 
 const App = () => {
-  return <Login />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/users" element={<UserList />} />
+        <Route path="*" element={<Navigate to="/Login" replace />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
